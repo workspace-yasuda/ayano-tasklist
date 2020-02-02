8fe3f329965ae021ed828dd3b139f9d4bfa68ef0
EAakiyama3104 <satoshi.akiyama@dragon.jp>
2020-02-02 17:16:31 +0900

    dockerfile 追加。動くように

--- Laravel_sample/config/filesystems.php
+++ Laravel_sample/config/filesystems.php
@@ -45,12 +45,12 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('_app'),
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('_app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
--- Laravel_sample/webpack.mix.js
+++ Laravel_sample/webpack.mix.js
@@ -11,5 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/_app.js', 'public/js')
+   .sass('resources/assets/sass/_app.scss', 'public/css');
