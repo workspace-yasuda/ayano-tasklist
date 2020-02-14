416f8e58fab91180eed3f924979a8ab8c204a981
ayano <>
2020-02-14 18:57:32 +0900

    photoController追加

---
+++ Laravel_sample/app/Http/Controllers/PhotoController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Request\StorePhoto;
+use App\Photo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facedes\Storage;
+
+class PhotoController extends Controller
+{
+
+  public function __construct()
+  {
+  $this->middleware('auth');
+  }
+
+  public function create(StorePhoto $request)
+  {
+    //投稿写真の拡張子を取得する
+    $extension = $request->photo->extension();
+
+    $photo =new Photo();
+
+    //インスタンス生成時に割り振られたランダム1D値と
+    //本来の拡張子を組み合わせてファイル名とする
+
+    $photo->filename = $photo->id .'.'. $extension;
+
+    //S3にファイルを保存
+    //第3引数の'public'はファイルを公開状態で保存するため
+    Storage::cloud()
+    ->putFileAs('',$request->photo,$photo->filename,'public');
+
+    //データベースエラー時にファイル削除を行うため
+    //トランザクションを利用する
+    DB::beginTransaction();
+
+    try{
+        Auth::user()->photos()->save($photo);
+        DB::commit();
+    }catch(\Exception $exception){
+      DB::rollback();
+      //DBとの不整合を避けるためアップロードしたファイルを削除
+      Storage::cloud()->delete($photo->filename);
+      throw $exception;
+    }
+
+    //リソースの新規作成なので
+    //レスポンスコードは201(CREATED)を返却する
+    return response($photo,201);
+
+  }
+}
--- Laravel_sample/app/Photo.php
+++ Laravel_sample/app/Photo.php
@@ -7,5 +7,39 @@ use Illuminate\Database\Support\Arr;
 
 class Photo extends Model
 {
-    //
+    protected $keyType = "string";
+
+    const ID_LENGTH =12;
+
+    public function __construct(array $attributes =[]){
+
+    parent::__construct($attributes);
+
+    if(! Arr:get($this->$attributes,"id")){
+      $this->setId();
+    }
+}
+
+private function setId(){
+  $this->attributes["id"] = $this->getRamdomId();
+}
+
+private function getRandomId(){
+
+  $characters = array_marge(
+    range(0,9),range("a","z"),
+    range("A","Z"),["-","_"]
+  );
+
+  $length = count($characters);
+
+  $id = "";
+
+  for($i=0;$i<self::ID_LENGTH;$i++){
+    $id=$characters[random_int(0,$length-1)];
+  }
+return $id;
+
+}
+
 }
