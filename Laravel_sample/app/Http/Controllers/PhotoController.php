c304d630cd515d068bc056c4d18bd9affc8702ba
ayano <>
2020-02-19 16:14:14 +0900

    インデント訂正

--- Laravel_sample/app/Http/Controllers/PhotoController.php
+++ Laravel_sample/app/Http/Controllers/PhotoController.php
@@ -14,7 +14,7 @@ class PhotoController extends Controller
 
   public function __construct()
   {
-  $this->middleware('auth');
+    $this->middleware('auth');
   }
 
   public function create(StorePhoto $request)
