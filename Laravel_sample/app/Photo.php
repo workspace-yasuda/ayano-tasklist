fefdceaa4d344740cac6d788df269760b2bd92a5
ayano <aya0516.y@gmail.com>
2020-02-19 23:22:21 +0900

    PhotoSubmitApiTest.php作成

--- Laravel_sample/app/Photo.php
+++ Laravel_sample/app/Photo.php
@@ -15,7 +15,7 @@ class Photo extends Model
 
     parent::__construct($attributes);
 
-    if(! Arr:get($this->$attributes,"id")){
+    if(! Arr::get($this->$attributes,"id")){
       $this->setId();
     }
 }
---
+++ Laravel_sample/tests/Feature/PhotoSubmitApiTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Photo;
+use App\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class PhotoSubmitApiTest extends TestCase
+{
+
+     public function setUp(): void
+   {
+       parent::setUp();
+
+
+       $this->user = factory(User::class)->create();
+   }
+
+    public function 写真をアップロードできること()
+    {
+        $this->assertTrue(true);
+
+        //ストレージオブジェクトのモックを作る
+        Storage::fake('s3');
+
+        //フェイクのストレージで画像をアップロードする
+        $response = $this->actingAs($this->user)
+        ->json('POST',route('photo.create'),
+        ['photo'=>UploadedFile::fake()->image('photo.png')]);
+
+        //アップロードできたか確認
+        $response->assertStatus(201);
+
+        //photoテーブルから保存したレコードを取得
+        $photo = Photo::first();
+
+        //レコードのファイル名が正しいか確認
+        $this->assertRegExp('/^[0-9a-zA-Z-_]{12}$/', $photo->id);
+
+        //ストレージに画像が入っているか確認
+        $cloud = Strage::cloud();
+        $cloud->asseartExists($photo->filename);
+    }
+}
